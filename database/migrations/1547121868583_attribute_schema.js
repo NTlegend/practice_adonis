965364cee10e623db106b217767023dd1267b77a
Webcosta <webkosta@gmail.com>
2019-01-10 14:04:58 +0200

    init

---
+++ database/migrations/1547121868583_attribute_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AttributeSchema extends Schema {
+  up () {
+    this.create('attributes', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('attributes')
+  }
+}
+
+module.exports = AttributeSchema
